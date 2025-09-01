e2496484fd3fbb4c4ff7bd2bada40bcd92f50147
HITXYR <xiaoyuru_30@163.com>
2025-09-01 14:28:44 +0800

    update

--- scripts/360_12.sh
+++ scripts/360_12.sh
@@ -4,5 +4,5 @@ base_path=./data/360
 scenes=("bicycle" "bicycle" "bonsai" "counter" "garden" "kitchen" "room" "stump")
 
 for scene in "${scenes[@]}"; do
-    bash scripts/train_eval_360_12.sh $base_path $scene $num_views sa
+    bash scripts/train_eval_360_12.sh $base_path $scene $num_views mcgs
 done
